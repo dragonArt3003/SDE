2b48cabc69f5f42d7877282b92031ba80c54f630
dragonArt3003 <84613548+dragonArt3003@users.noreply.github.com>
2021-06-22 21:42:48 +0530

    Create d15B.cpp

---
+++ Daily Practice/d15B.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        bool last = false;
+        
+        std::queue<TreeNode*> que;
+        que.push(root);
+        
+        while (!que.empty()){
+            TreeNode *cur = que.front();
+            que.pop();
+            
+            if (cur == nullptr)
+                last = true;
+            else{
+                if (last == true)   return false;
+                que.push(cur->left);
+                que.push(cur->right);
+            }
+        }
+        return true;
+    }
+};
